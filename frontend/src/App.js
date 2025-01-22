51baf5d2408beb92cf2f66c26bbb7febbab99ac2
root <root@mihirscomputer.>
2025-01-22 15:32:48 -0800

    Fixed margins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
 import Recipes from './components/Recipes';
-
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
 function App() {
   return (
     <>
@@ -15,11 +15,44 @@ function App() {
       <BrowserRouter>
       <Navbar />
         <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/services" exact element={<Services />} />
-            <Route path="/recipes" exact element={<Recipes />} />
-            <Route path="/sign-up" exact element={<SignUp />} />
 
+          <Route path="/" exact element={<Home />} />
+          {/* Protected Route for Services */}
+          <Route
+            path="/services"
+            exact
+            element={
+              <>
+                <SignedIn>
+                  <Services />
+                </SignedIn>
+                <SignedOut>
+                  <div className="error-message">
+                    <h1>Error: Must Be Signed In</h1>
+                  </div>
+                </SignedOut>
+              </>
+            }
+          />
+          
+          {/* Protected Route for Recipes */}
+          <Route
+            path="/recipes"
+            exact
+            element={
+              <>
+                <SignedIn>
+                  <Recipes />
+                </SignedIn>
+                <SignedOut>
+                  <div className="error-message">
+                    <h1>Error: Must Be Signed In</h1>
+                  </div>
+                </SignedOut>
+              </>
+            }
+          />
+          <Route path="/sign-up" exact element={<SignUp />} />
         </Routes>
       </BrowserRouter>
 
